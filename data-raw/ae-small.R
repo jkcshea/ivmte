275ab4b96220ddeee3e64e48705c92291738b22e
A. Torgovitsky <atorgovitsky@gmail.com>
2019-09-05 15:25:22 -0500

    Added Angrist-Evans data.

---
+++ data-raw/ae-small.R
@@ -0,0 +1,25 @@
+# The original data in Stata format is available here:
+#
+# http://sites.bu.edu/ivanf/files/2014/03/m_d_806.dta_.zip
+#
+# That file is cleaned using this Stata code:
+#
+# http://sites.bu.edu/ivanf/files/2014/03/code.zip
+#
+# We have run this and saved it in .csv format, which this script downloads.
+# The file is around 80 MB, so not included with the package.
+# This script creates smaller version by removing unneeded columns.
+rm(list = ls())
+
+#### WAITING FOR WEB SPACE FROM U CHICAGO
+
+#download.file("http://sites.bu.edu/ivanf/files/2014/03/m_d_806.dta_.zip",temp)
+#aedata <- read_dta(unz(temp, "m_d_806.dta"))
+#unlink(temp)
+
+ae = read.csv("1980-AllWomen.csv")
+ae <- subset(ae, agefstm >= 20)
+ae <- subset(ae, select=c(workedm, hourswm, morekids, samesex, YOBM))
+colnames(ae) <- c("worked", "hours", "morekids", "samesex", "yob")
+library("devtools")
+use_data(ae, overwrite = TRUE)
